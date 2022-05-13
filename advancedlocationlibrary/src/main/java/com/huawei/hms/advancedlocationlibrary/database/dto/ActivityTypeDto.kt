261638128af8e5cbebac73f00753f3b2eab09738
b00636587 <begum.avci1@huawei.com>
2022-05-13 12:03:38 +0300

    Added activity identify methods

---
+++ advancedlocationlibrary/src/main/java/com/huawei/hms/advancedlocationlibrary/database/dto/ActivityTypeDto.kt
@@ -0,0 +1,10 @@
+package com.huawei.hms.advancedlocationlibrary.database.dto
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tbl_activity_type")
+data class ActivityTypeDto(
+    @PrimaryKey(autoGenerate = true) var activity_type_id : Int = 0,
+    var activityTypeCode: Int?
+)
